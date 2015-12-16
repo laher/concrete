bd1d3971393c4f2e5d445ff53f237962694e64fc
Am Laher <am.laher@hapara.com>
2015-12-17 08:45:32 +1300

    basic version is working, with flags

--- concrete.go
+++ concrete.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"flag"
 	"fmt"
 	"go/ast"
 	"go/importer"
@@ -9,14 +11,63 @@ import (
 	"go/types"
 	"log"
 	"os"
+	"text/template"
 )
 
+var (
+	interfaceName   = flag.String("interface", "", "The name of the interface to be implemented")
+	filename        = flag.String("in-file", "", "file where interface is defined")
+	implPackage     = flag.String("impl-package", "", "package")
+	concreteTypeTpl = flag.String("implementation", "{{.Interface}}Impl", "The name of the concrete implementation")
+	helpFlag        = flag.Bool("help", false, "show detailed help message")
+	writeFlag       = flag.Bool("w", false, "rewrite input files in place (by default, the results are printed to standard output)")
+	verboseFlag     = flag.Bool("v", false, "show verbose matcher diagnostics")
+)
+
+const usage = `concrete: a tool to implement interfaces
+
+Usage: concrete -interface <InterfaceName> -in-file <existing-file.go> -impl-package <package> [options]
+
+-in-file         existing file which contains the interface
+-interface       name of interface
+-impl-package    package name of existing interface
+-concrete        The name of the implementation. Uses templates. default '{{.Interface}}Impl'
+-help            show detailed help message
+`
+
 func main() {
-	interfaceName := os.Args[1]
-	concreteType := os.Args[2]
-	filename := os.Args[3]
-	pkg := os.Args[4]
-	parseAndPrintFile(filename, interfaceName, concreteType, pkg)
+	if err := doMain(); err != nil {
+		fmt.Fprintf(os.Stderr, "eg: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+func doMain() error {
+	flag.Parse()
+
+	if *helpFlag || *filename == "" {
+		fmt.Fprint(os.Stderr, usage)
+		os.Exit(2)
+	}
+
+	tmpl, err := template.New("concreteTypeTemplate").Parse(*concreteTypeTpl)
+	if err != nil {
+		fmt.Printf("Error parsing template")
+		return err
+	}
+	data := struct {
+		Interface string
+	}{
+		Interface: *interfaceName,
+	}
+	var out bytes.Buffer
+	err = tmpl.Execute(&out, data)
+	if err != nil {
+		fmt.Printf("Error processing template")
+		return err
+	}
+	parseAndPrintFile(*filename, *interfaceName, out.String(), *implPackage)
+	return nil
 }
 
 func parseAndPrintFile(filename, interfaceName, concreteType, pkg string) {
